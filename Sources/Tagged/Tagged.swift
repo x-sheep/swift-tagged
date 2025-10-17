ca7b335b17d4060ee179e52dae2b673093891a71
Lennard Sprong <lennard@buku.io>
2025-10-17 09:30:15 +0200

    Add SendableMetatype conditional conformance

--- Sources/Tagged/Tagged.swift
+++ Sources/Tagged/Tagged.swift
@@ -113,6 +113,10 @@ extension Tagged: Sendable where RawValue: Sendable {}
 extension Tagged: BitwiseCopyable where RawValue: BitwiseCopyable {}
 #endif
 
+#if swift(>=6.2)
+extension Tagged: SendableMetatype where RawValue: SendableMetatype {}
+#endif
+
 extension Tagged: ExpressibleByBooleanLiteral where RawValue: ExpressibleByBooleanLiteral {
   public init(booleanLiteral value: RawValue.BooleanLiteralType) {
     self.init(rawValue: RawValue(booleanLiteral: value))
